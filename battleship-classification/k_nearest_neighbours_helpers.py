f8a973105bec46f9d13502dfa6dd7016a74f43ad
Alex Gooding <alexxgooding@gmail.com>
2019-10-10 17:15:12 +0100

    initial more useful variables

--- battleship-classification/k_nearest_neighbours_helpers.py
+++ battleship-classification/k_nearest_neighbours_helpers.py
@@ -2,10 +2,13 @@ from scipy.spatial import distance
 
 class k_nearest_neighbours_helper:
 
-    def __init__(self, labels, data_points):
+    def __init__(self, labels, data_points, k):
         # list of the labels corresponding to each data point
         self.labels = labels
-        self.data_point = data_points
+        self.data_points = data_points
+        self.label_names = list(dict.fromkeys(self.labels))
+        self.data_point_dict = dict(zip(self.data_points, self.labels))
+        self.k = k
 
     @staticmethod
     def calculate_euclidean_distance(point_1, point_2):
