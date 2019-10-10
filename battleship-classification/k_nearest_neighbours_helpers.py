6107677ae8385311c26c0425e41ca419f1b9dd62
Alex Gooding <alexxgooding@gmail.com>
2019-10-10 16:37:10 +0100

    create method to calculate distance between two points

--- battleship-classification/k_nearest_neighbours_helpers.py
+++ battleship-classification/k_nearest_neighbours_helpers.py
@@ -1,7 +1,13 @@
-
+from scipy.spatial import distance
 
 class k_nearest_neighbours_helper:
 
     def __init__(self, labels, data_points):
+        # list of the labels corresponding to each data point
         self.labels = labels
         self.data_point = data_points
+
+    @staticmethod
+    def calculate_euclidean_distance(point_1, point_2):
+        return distance.euclidean(point_1, point_2)
+
