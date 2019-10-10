d2b9f6070abc5071752123908ee50d0b6d5240b1
Alex Gooding <alexxgooding@gmail.com>
2019-10-10 18:37:25 +0100

    Draw a simple 2d plot to show our example visually

--- battleship-classification/battleship_classification.py
+++ battleship-classification/battleship_classification.py
@@ -1,4 +1,28 @@
 from k_nearest_neighbours_helper import k_nearest_neighbours_helper
+import matplotlib.pyplot as plt
+
+"""
+A function to show our simple battleships example more clearly
+"""
+def create_2d_plot_for_two_classifiers(data_dict, label_names, new_points):
+    data_points_1 = []
+    data_points_2 = []
+    for point, label in data_dict.items():
+        if label == label_names[0]:
+            data_points_1.append(point)
+        else:
+            data_points_2.append(point)
+    data_points_1_x = [point[0] for point in data_points_1]
+    data_points_1_y = [point[1] for point in data_points_1]
+    data_points_2_x = [point[0] for point in data_points_2]
+    data_points_2_y = [point[1] for point in data_points_2]
+    plt.plot(data_points_1_x, data_points_1_y, 'bo')
+    plt.plot(data_points_2_x, data_points_2_y, 'ro')
+    for i in range(0, len(new_points)):
+        plt.annotate("New Point " + str(i+1), xy=new_points[i], xytext=(-15, 25), textcoords='offset points',
+            arrowprops=dict(facecolor='black', shrink=0.05),
+            horizontalalignment='right', verticalalignment='bottom')
+    plt.show()
 
 if __name__ == "__main__":
     ship_locations = [(0, 1), (3, 2), (2, 2), (4, 3), (8, 9), (10, 10), (7, 9), (6, 5)]
@@ -8,7 +32,9 @@ if __name__ == "__main__":
     new_point_1 = (1, 2)
     new_point_2 = (8, 8)
     new_point_3 = (5, 4)
-    helper.classify_new_point(new_point_1)
-    helper.classify_new_point(new_point_2)
-    helper.classify_new_point(new_point_3)
+    list_of_new_points = [new_point_1, new_point_2, new_point_3]
+    for point in list_of_new_points:
+        helper.classify_new_point(point)
     helper.print_data()
+    create_2d_plot_for_two_classifiers(helper.data_point_dict, helper.label_names, list_of_new_points)
+
