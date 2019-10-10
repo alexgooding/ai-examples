423a4803dfa7a8b22094fb2213ce5913373a06cc
Alex Gooding <alexxgooding@gmail.com>
2019-10-10 17:24:14 +0100

    create algorithm to classify a new point

--- battleship-classification/k_nearest_neighbours_helpers.py
+++ battleship-classification/k_nearest_neighbours_helpers.py
@@ -1,4 +1,5 @@
 from scipy.spatial import distance
+import operator
 
 """
 A helper class for running the k-nearest neighbours algorithm.
@@ -18,3 +19,25 @@ class k_nearest_neighbours_helper:
     def calculate_euclidean_distance(point_1, point_2):
         return distance.euclidean(point_1, point_2)
 
+    def vote_for_label(self, points):
+        votes_counted = dict(zip(self.label_names, len(self.label_names)*[0]))
+        for point in points:
+            votes_counted[self.data_point_dict[point]] += 1
+        # return the label with the most votes
+        return max(votes_counted.items(), key=operator.itemgetter(1))[0]
+
+    def classify_new_point(self, new_point):
+        distance_dict = dict(zip(self.data_points, len(self.data_points)*[None]))
+        # calculate distance from each point to the new point
+        for point in distance_dict.keys():
+            distance_dict[point] = self.calculate_euclidean_distance(point, new_point)
+        # extract a sorted list of data points based on proximity to the new point
+        sorted_dict = sorted(distance_dict.items(), key=operator.itemgetter(1))
+        k_nearest_neighbours = []
+        for i in range(self.k):
+            k_nearest_neighbours.append(sorted_dict[i][0])
+        new_label = self.vote_for_label(k_nearest_neighbours)
+        # add the new data point and label to the exist lists
+        self.data_points.append(new_point)
+        self.labels.append(new_label)
+        self.data_point_dict = dict(zip(self.data_points, self.labels))
