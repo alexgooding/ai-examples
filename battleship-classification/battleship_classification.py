59e6b9df79577e7b3ef8c5802562c576b14ba177
Alex Gooding <alexxgooding@gmail.com>
2019-10-11 15:43:59 +0100

    add circles to indicate nearest neighbours used to graph

--- battleship-classification/battleship_classification.py
+++ battleship-classification/battleship_classification.py
@@ -5,7 +5,8 @@ import random
 """
 A function to show our simple battleships example more clearly
 """
-def create_2d_plot_for_two_classifiers(data_dict, label_names, new_points):
+def create_2d_plot_for_two_classifiers(data_dict, label_names, new_points, distances_to_furthest_neighbour):
+    # Prepare data for plotting
     data_points_1 = []
     data_points_2 = []
     for point, label in data_dict.items():
@@ -17,25 +18,30 @@ def create_2d_plot_for_two_classifiers(data_dict, label_names, new_points):
     data_points_1_y = [point[1] for point in data_points_1]
     data_points_2_x = [point[0] for point in data_points_2]
     data_points_2_y = [point[1] for point in data_points_2]
-    plt.plot(data_points_1_x, data_points_1_y, 'bo')
-    plt.plot(data_points_2_x, data_points_2_y, 'ro')
+    fig, ax = plt.subplots()
+    # Plot the two classes of points
+    ax.plot(data_points_1_x, data_points_1_y, 'bo')
+    ax.plot(data_points_2_x, data_points_2_y, 'ro')
+    # Annotate the new points on the graph
     for i in range(0, len(new_points)):
-        plt.annotate("New Point " + str(i+1), xy=new_points[i], xytext=(-15, 25), textcoords='offset points',
+        ax.annotate("New Point " + str(i+1), xy=new_points[i], xytext=(-15, 25), textcoords='offset points',
             arrowprops=dict(facecolor='black', shrink=0.05),
             horizontalalignment='right', verticalalignment='bottom')
-    plt.show()
+    # Draw on circles to indicate which neighbours were selected
+    for i in range(len(new_points)):
+        ax.add_artist(plt.Circle(new_points[i], distances_to_furthest_neighbour[i], color='g'))
+    fig.savefig('k_nearest_neighbours_graph.png')
+    fig.show()
 
 if __name__ == "__main__":
     ship_locations = [(random.random() * 10.0, random.random() * 10.0) for _ in range(100)]
     ship_labels = ["Axis"] * 50 + ["Allies"] * 50
     k = 3
     helper = k_nearest_neighbours_helper(ship_locations, ship_labels, k)
-    new_point_1 = (1, 2)
-    new_point_2 = (8, 8)
-    new_point_3 = (5, 4)
-    list_of_new_points = [new_point_1, new_point_2, new_point_3]
+    list_of_new_points = [(1, 2), (8, 8), (5, 4)]
+    distances_to_furthest_neighbour = []
     for point in list_of_new_points:
-        helper.classify_new_point(point)
+        distances_to_furthest_neighbour.append(helper.classify_new_point(point))
     helper.print_data()
-    create_2d_plot_for_two_classifiers(helper.data_point_dict, helper.label_names, list_of_new_points)
+    create_2d_plot_for_two_classifiers(helper.data_point_dict, helper.label_names, list_of_new_points, distances_to_furthest_neighbour)
 
